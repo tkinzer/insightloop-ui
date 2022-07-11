0f4e5bef9210b15e9ca886e21307e50ca4b2af13
Tim <timothykinzer@gmail.com>
2022-07-11 12:14:21 -0700

    add login api

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -5,16 +5,60 @@ import { SignInButton } from '~/components/domain/auth/SignInButton';
 import { SignOutButton } from '~/components/domain/auth/SignOutButton';
 import { Head } from '~/components/shared/Head';
 import { Splash } from '~/components/screens/Splash';
+import { getAuth, GoogleAuthProvider, signInAnonymously, signInWithPopup, signInWithRedirect } from 'firebase/auth';
 
 function Index() {
   const { state } = useUserState();
   const navigate = useNavigate();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
   const handleComplete = () => {
     // if (completeButtonRef.current) completeButtonRef.current.focus();
     console.log(`User is ${state ? 'signed in' : 'signed out'}`, state);
     // completeButtonRef.current.focus();
     navigate('/journal');
+  };
+
+  const loginGuest = () => {
+    async () => {
+      const result = await signInAnonymously(auth);
+      if (result) {
+        console.log('login with google reuslt');
+        console.log(result);
+      }
+    };
+  };
+
+  const loginWithGoogle = () => {
+    async () => {
+      const result = await signInWithRedirect(auth, provider);
+      if (result) {
+        console.log('login with google', result);
+      }
+    };
+  };
+
+  const loginUser = () => {
+    async () =>
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          console.log('user login', result);
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential?.accessToken;
+          console.log('signed in user:  ', token);
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.customData.email;
+          // The AuthCredential type that was used.
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          console.error(credential);
+        });
   };
 
   return (
@@ -31,7 +75,7 @@ function Index() {
               <SignOutButton />
             )}
             {/* <Navigate to="/home" /> */}
-            <button onClick={handleComplete}>Continue as Guest</button>
+            <button onClick={loginGuest}>Continue as Guest</button>
           </div>
         </div>
       </div>
